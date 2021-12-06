bbfe58dc64c357f2cab74ab657ac87f7e76cda5b
Benjamin Parsons <90803531+the-earth-31@users.noreply.github.com>
2021-12-06 17:52:31 -0500

    Add files via upload
    RaccBot Version 1.1 with added gifs

---
+++ RaccBot1.1.js
@@ -0,0 +1,80 @@
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+process.on('unhandledRejection', error => {
+  console.error(
+    "There was an error! Did you update the config.json file? " +
+    "You messed something up. \n",
+    error
+  );
+  process.exit();
+});
+
+client.on('ready', () => {
+  console.log(client.user.tag)
+});
+
+client.on('message', async message => {
+  if (message.content.startsWith('&yo')) {
+
+    message.reply(`Wassup it is me, racc bot`); //Line (Inline) Reply without mention
+  }
+
+  if (message.content.startsWith('&sad')) {
+  	message.reply(`https://tenor.com/view/bobatyun-raccoon-soobin-gif-19369912`);
+  }
+
+  if (message.content.startsWith('&speed')) {
+  	message.reply(`https://tenor.com/view/racoon-gif-22278566`);
+  }
+
+  if (message.content.startsWith('&wind')) {
+  	message.reply(`https://tenor.com/view/racoon-happy-ahhh-joy-ride-windy-gif-10015381`);
+  }
+
+  if (message.content.startsWith('&water')) {
+  	message.reply(`https://tenor.com/view/raccoon-playing-water-pool-pet-gif-7564469`);
+  }
+
+  if (message.content.startsWith('&high five')) {
+  	message.reply(`https://tenor.com/view/high-five-raccoons-funny-animals-trash-pandas-gif-11969111`);
+  }
+
+    if (message.content.startsWith('&food')) {
+  	message.reply(`https://tenor.com/view/eating-the-pet-collective-yummy-popcorn-raccoon-gif-16894090`);
+  }
+    
+    if (message.content.startsWith('&chonk')) {
+  	message.reply(`https://tenor.com/view/scooch-raccoon-chubby-chonk-absolute-unit-gif-14789220`);
+  }
+
+    if (message.content.startsWith('&hug')) {
+  	message.reply(`https://tenor.com/view/v-a-sly-racoon-gif-20903341`);
+  }
+
+    if (message.content.startsWith('&sleep')) {
+  	message.reply(`https://tenor.com/view/raccoon-retreat-withdraw-bed-hide-gif-18813480`);
+  }
+
+   if (message.content.startsWith('&evil')) {
+    message.reply(`https://cdn.discordapp.com/attachments/737364590618345482/917539399502295040/unknown.gif`);
+  }
+
+    if (message.content.startsWith('&bread')) {
+    message.reply(`https://tenor.com/view/raccoon-bread-raccoon-bread-raccoon-with-bread-racco-gif-22120699`);
+  }
+
+    if (message.content.startsWith('&shop')) {
+    message.reply(`https://cdn.discordapp.com/attachments/737364590618345482/917539715551461416/unknown.gif`);
+  }
+
+    if (message.content.startsWith('&piano')) {
+    message.reply(`https://cdn.discordapp.com/attachments/737364590618345482/917539866512871484/unknown.gif`);
+  }
+
+
+});
+
+client.login('#token here');
